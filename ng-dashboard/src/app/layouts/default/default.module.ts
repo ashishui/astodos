765f165f3283f7fb2ced55064c6d64289cd1f4f3
ashishui <ashishmishra221984@gmail.com>
2020-10-19 16:03:22 +0530

    simple side bar created

---
+++ ng-dashboard/src/app/layouts/default/default.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [DefaultComponent],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports: [DefaultComponent]
+})
+export class DefaultModule { }
